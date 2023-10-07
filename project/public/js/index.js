fa1cc0d2c10e3e57205dd271ba5db564c2765f6b
leOhsantos <leonardo.santos191004@gmail.com>
2023-10-07 20:56:34 -0300

    updating sign in btn and background color

--- project/public/js/index.js
+++ project/public/js/index.js
@@ -1,5 +1,3 @@
 const nav = document.querySelector(".nav");
-const signInBtn = document.getElementById("signInBtn");
 
 window.addEventListener("scroll", () => nav.classList.toggle("active", scrollY > 0));
-signInBtn.addEventListener("click", () => window.location.href = "auth.html#signIn");
