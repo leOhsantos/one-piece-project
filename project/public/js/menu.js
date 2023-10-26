79cbf65fe8a0b5eba7b2c8d9ebf2e2db414d8177
leOhsantos <leonardo.santos191004@gmail.com>
2023-10-26 18:34:49 -0300

    adding confirm logout modal

--- project/public/js/menu.js
+++ project/public/js/menu.js
@@ -1,5 +1,23 @@
+const background = document.querySelector(".background");
+const confirmLogoutModal = document.querySelector(".confirm-logout");
 const settingsBtn = document.getElementById("settingsBtn");
+const confirmLogoutBtn = document.getElementById("confirmLogoutBtn");
+const cancelLogoutBtn = document.getElementById("cancelLogoutBtn");
 const logoutBtn = document.getElementById("logoutBtn");
+
+function openConfirmLogoutModal() {
+    background.classList.add("active");
+    confirmLogoutModal.classList.add("active");
+}
+
+confirmLogoutBtn.addEventListener("click", openConfirmLogoutModal);
+
+function closeConfirmLogoutModal() {
+    background.classList.remove("active");
+    confirmLogoutModal.classList.remove("active");
+}
+
+cancelLogoutBtn.addEventListener("click", closeConfirmLogoutModal);
 
 function logoutUser() {
     window.location.href = "../index.html";
