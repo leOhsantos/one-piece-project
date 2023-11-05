11f0df0c405a505ffcaba161ed247795e2df075e
leOhsantos <leonardosantos191004@gmail.com>
2023-11-05 17:25:36 -0300

    updating settings container

--- project/public/js/menu.js
+++ project/public/js/menu.js
@@ -3,28 +3,90 @@ const background = document.querySelector(".background");
 const gameMenuBtn = document.getElementById("gameMenuBtn");
 const statisticsMenuBtn = document.getElementById("statisticsMenuBtn");
 
-const containerSettings = document.querySelector(".container-settings");
+const settingsContainer = document.querySelector(".settings-container");
 const settingsBtn = document.getElementById("settingsBtn");
 const closeSettingsBtn = document.getElementById("closeSettingsBtn");
+const saveEditionBtn = document.getElementById("saveEditionBtn");
+
+const avatarContainer = document.querySelector(".avatar-container");
+const avatarBtn = document.getElementById("avatarBtn");
+
+const accountContainer = document.querySelector(".account-container");
+const accountBtn = document.getElementById("accountBtn");
+
+const securityContainer = document.querySelector(".security-container");
+const securityBtn = document.getElementById("securityBtn");
+const currentPassword = document.getElementById("currentPasswordInput");
+const newPassword = document.getElementById("newPasswordInput");
+const confirmNewPassword = document.getElementById("confirmNewPasswordInput");
 
 const confirmLogoutModal = document.querySelector(".confirm-logout");
 const confirmLogoutBtn = document.getElementById("confirmLogoutBtn");
 const cancelLogoutBtn = document.getElementById("cancelLogoutBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 
-function openContainerSettings() {
+function openAvatarContainer() {
+    avatarContainer.classList.add("active");
+    accountContainer.classList.remove("active");
+    securityContainer.classList.remove("active");
+    saveEditionBtn.setAttribute("onclick", "saveAvatarEdition()");
+    cleanSecurityInputs();
+}
+
+if (avatarBtn) avatarBtn.addEventListener("click", openAvatarContainer);
+
+function openAccountContainer() {
+    avatarContainer.classList.remove("active");
+    accountContainer.classList.add("active");
+    securityContainer.classList.remove("active");
+    saveEditionBtn.setAttribute("onclick", "saveAccountEdition()");
+    cleanSecurityInputs();
+}
+
+if (accountBtn) accountBtn.addEventListener("click", openAccountContainer);
+
+function openSecurityContainer() {
+    avatarContainer.classList.remove("active");
+    accountContainer.classList.remove("active");
+    securityContainer.classList.add("active");
+    saveEditionBtn.setAttribute("onclick", "saveSecurityEdition()");
+    currentPassword.focus();
+}
+
+if (securityBtn) securityBtn.addEventListener("click", openSecurityContainer);
+
+function cleanSecurityInputs() {
+    currentPassword.value = "";
+    newPassword.value = "";
+    confirmNewPassword.value = "";
+}
+
+function saveAvatarEdition() {
+    closeSettingsContainer();
+}
+
+function saveAccountEdition() {
+    closeSettingsContainer();
+}
+
+function saveSecurityEdition() {
+    closeSettingsContainer();
+}
+
+function openSettingsContainer() {
     background.classList.add("active");
-    containerSettings.classList.add("active");
+    settingsContainer.classList.add("active");
+    openAvatarContainer();
 }
 
-if (settingsBtn) settingsBtn.addEventListener("click", openContainerSettings);
+if (settingsBtn) settingsBtn.addEventListener("click", openSettingsContainer);
 
-function closeContainerSettings() {
+function closeSettingsContainer() {
     background.classList.remove("active");
-    containerSettings.classList.remove("active");
+    settingsContainer.classList.remove("active");
 }
 
-if (closeSettingsBtn) closeSettingsBtn.addEventListener("click", closeContainerSettings);
+if (closeSettingsBtn) closeSettingsBtn.addEventListener("click", closeSettingsContainer);
 
 function openConfirmLogoutModal() {
     background.classList.add("active");
